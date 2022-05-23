87ba63c938a90569ce770a1cbb14ac34b342462e
Juan Tapia <tapiahuanca.juanluis@gmail.com>
2022-05-23 20:43:52 -0300

    second commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hola mundo");
+            Console.WriteLine("hola mundo mudificado");
             Console.ReadKey(true);
         }
     }
