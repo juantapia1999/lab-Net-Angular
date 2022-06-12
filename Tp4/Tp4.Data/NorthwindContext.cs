b43dd56c1ead76cda24c2d076531689b4439feda
Juan Tapia <tapiahuanca.juanluis@gmail.com>
2022-06-12 16:50:23 -0300

    change namespace in Tp4.Data

--- Tp4/Tp4.Data/NorthwindContext.cs
+++ Tp4/Tp4.Data/NorthwindContext.cs
@@ -4,7 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using Tp4.Entities;
 
-namespace Tp4.Entities
+namespace Tp4.Data
 {
     public partial class NorthwindContext : DbContext
     {
@@ -13,30 +13,11 @@ namespace Tp4.Entities
         {
         }
 
-        //public virtual DbSet<Products> Products { get; set; }
-        //public virtual DbSet<Region> Region { get; set; }
         public virtual DbSet<Shippers> Shippers { get; set; }
-        //public virtual DbSet<Territories> Territories { get; set; }
         public virtual DbSet<Categories> Categories { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Products>()
-            //    .Property(e => e.UnitPrice)
-            //    .HasPrecision(19, 4);
-
-            //modelBuilder.Entity<Region>()
-            //    .Property(e => e.RegionDescription)
-            //    .IsFixedLength();
-
-            //modelBuilder.Entity<Region>()
-            //    .HasMany(e => e.Territories)
-            //    .WithRequired(e => e.Region)
-            //    .WillCascadeOnDelete(false);
-
-            //modelBuilder.Entity<Territories>()
-            //    .Property(e => e.TerritoryDescription)
-            //    .IsFixedLength();
         }
     }
 }
--- Tp4/Tp4.Logic/BaseLogic.cs
+++ Tp4/Tp4.Logic/BaseLogic.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tp4.Entities;
+using Tp4.Data;
 
 namespace Tp4.Logic
 {
