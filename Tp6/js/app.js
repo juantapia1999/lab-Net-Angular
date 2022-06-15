c52687e1b9dbac3ab98b17a9034c4ea2d9888300
Juan Tapia <tapiahuanca.juanluis@gmail.com>
2022-06-15 16:54:40 -0300

    final

--- Tp6/js/app.js
+++ Tp6/js/app.js
@@ -2,6 +2,9 @@
 document.getElementById("fechaNacimiento").addEventListener("change", calcularEdad);
 
 function calcularEdad() {
+
+    getEdad();
+
     let fechaNacimiento = new Date(document.getElementById("fechaNacimiento").value);
     let fechaHoy = new Date();
 
@@ -28,37 +31,43 @@ function calcularEdad() {
                         edad.min = minutos;
                     } else {
                         edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
+                        edad.min = 60 + minutos;
                     }
                 } else {
-                    edad.dia = dias - 1;
-                    edad.hora = fechaHoy.getHours() +1;
+                    edad.dia = ((fechaHoy.getDate()) + (fechaNacimiento.getDate())) - 1;
+                    edad.hora = fechaHoy.getHours() + 1;
                     if (minutos >= 0) {
                         edad.min = minutos;
                     } else {
                         edad.hora = fechaHoy.getHours();
-                        edad.min = fechaHoy.getMinutes() + fechaNacimiento.getMinutes() ;
+                        edad.min = fechaHoy.getMinutes() + fechaNacimiento.getMinutes();
                     }
                 }
             } else {
-                edad.mes = meses - 1;
+                edad.año = años - 1;
+                edad.mes = ((fechaHoy.getMonth()) + (fechaNacimiento.getMonth())) + 1;
                 edad.dia = ((fechaHoy.getDate()) + (fechaNacimiento.getDate())) - 1;
+                if (edad.dia > 31) {
+                    edad.dia = edad.dia + dias;
+                    edad.mes = meses;
+                    edad.año = años;
+                }
                 if (horas >= 0) {
                     edad.hora = horas;
                     if (minutos >= 0) {
                         edad.min = minutos;
                     } else {
                         edad.hora = fechaHoy.getHours();
-                        edad.min = fechaHoy.getMinutes() + fechaNacimiento.getMinutes() ;
+                        edad.min = fechaHoy.getMinutes() + fechaNacimiento.getMinutes();
                     }
                 } else {
-                    edad.dia = ((fechaHoy.getDate()) + (fechaNacimiento.getDate()))-1;
-                    edad.hora = fechaHoy.getHours() +1;
+                    edad.dia = ((fechaHoy.getDate()) + (fechaNacimiento.getDate())) - 1;
+                    edad.hora = fechaHoy.getHours() + 1;
                     if (minutos >= 0) {
                         edad.min = minutos;
                     } else {
                         edad.hora = fechaHoy.getHours();
-                        edad.min = fechaHoy.getMinutes() + fechaNacimiento.getMinutes() ;
+                        edad.min = fechaHoy.getMinutes() + fechaNacimiento.getMinutes();
                     }
                 }
             }
@@ -97,107 +106,68 @@ function calcularEdad() {
                         edad.min = (minutos * -1);
                     }
                 } else {
-                    edad.dia = dias - 1;
-                    edad.hora = (horas * -1);
+                    edad.dia = (fechaHoy.getDate()) + (fechaNacimiento.getDate()) - 1;
+                    edad.hora = horas * -1;
                     if (minutos >= 0) {
                         edad.min = minutos;
                     } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                }
-            }
-        }
-    } else {
-        if (meses >= 0) {
-            edad.mes = meses;
-            if (dias >= 0) {
-                edad.dia = dias;
-                if (horas >= 0) {
-                    edad.hora = horas;
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                } else {
-                    edad.dia = dias - 1;
-                    edad.hora = (horas * -1);
-                }
-            } else {
-                edad.mes = meses - 1;
-                edad.dia = (dias * -1);
-                if (horas >= 0) {
-                    edad.hora = horas;
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                } else {
-                    edad.dia = dias - 1;
-                    edad.hora = (horas * -1);
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                }
-            }
-        } else {
-            edad.año = años - 1;
-            edad.mes = (meses * -1);
-            if (dias >= 0) {
-                edad.dia = dias;
-                if (horas >= 0) {
-                    edad.hora = horas;
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                } else {
-                    edad.dia = dias - 1;
-                    edad.hora = (horas * -1);
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                }
-            } else {
-                edad.mes = meses - 1;
-                edad.dia = (dias * -1);
-                if (horas >= 0) {
-                    edad.hora = horas;
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
-                        edad.min = (minutos * -1);
-                    }
-                } else {
-                    edad.dia = dias - 1;
-                    edad.hora = (horas * -1);
-                    if (minutos >= 0) {
-                        edad.min = minutos;
-                    } else {
-                        edad.hora = horas - 1;
+                        edad.hora = 60 + horas;
                         edad.min = (minutos * -1);
                     }
                 }
             }
         }
     }
-    console.log(`${fechaHoy.getFullYear()} ${fechaHoy.getMonth()} ${fechaHoy.getDate()} ${fechaHoy.getHours()} ${fechaHoy.getMinutes()}`);
-    console.log(`${fechaNacimiento.getFullYear()} ${fechaNacimiento.getMonth()} ${fechaNacimiento.getDate()} ${fechaNacimiento.getHours()} ${fechaNacimiento.getMinutes()}`);
-    console.log(`${años} ${meses} ${dias} ${horas} ${minutos}`)
-    console.log("calculado: ")
-    console.log(edad)
-    document.getElementById("edad").textContent;
+
+    if (edad.año < 0 && edad.mes < 0 && edad.dia < 0 && edad.min < 0) {
+        alert("¿Vienes del futuro eh?")
+        return;
+    }
+    //console.log(`${fechaHoy.getFullYear()} ${fechaHoy.getMonth()} ${fechaHoy.getDate()} ${fechaHoy.getHours()} ${fechaHoy.getMinutes()}`);
+    //console.log(`${fechaNacimiento.getFullYear()} ${fechaNacimiento.getMonth()} ${fechaNacimiento.getDate()} ${fechaNacimiento.getHours()} ${fechaNacimiento.getMinutes()}`);
+    //console.log(`${años} ${meses} ${dias} ${horas} ${minutos}`)
+    //console.log("calculado: ")
+    //console.log(edad)
+    document.getElementById("edadExperimental").textContent = `años: ${edad.año}, meses: ${edad.mes}, dias: ${edad.dia}, horas: ${edad.hora}, minutos: ${edad.min}`;
+}
+
+function getEdad() {
+    let hoy = new Date()
+    let fechaNacimiento = new Date(document.getElementById("fechaNacimiento").value);
+    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear()
+    let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth()
+    if (diferenciaMeses < 0 || (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+        edad--
+    }
+    document.getElementById("edadExacta").textContent = edad + " años";
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let fechaNacimiento = (document.getElementById("fechaNacimiento").value).replace('T', '  ');
+    let edadExacta = document.getElementById("edadExacta").textContent;
+    let edadExperimental = document.getElementById("edadExperimental").textContent;
+    let empresa = document.getElementById("empresa").value;
+    let op = {
+        m: document.getElementById("op1").checked,
+        f: document.getElementById("op2").checked,
+        o: document.getElementById("op3").checked
+    }
+    let sexo = op.m ? "masuclino" : (op.f ? "femenino" : "otro");
+    let message = `Nombre: ${nombre}\nApellido: ${apellido}\nFecha de Nacimiento: ${fechaNacimiento}\nEdad exacta: ${edadExacta}\nEdad experimental: ${edadExperimental}\nSexo: ${sexo}\nEmpresa: ${empresa}`;
+    alert(message);
+}
+
+function resetForm() {
+    document.getElementById("nombre").value="";
+    document.getElementById("apellido").value="";
+    document.getElementById("fechaNacimiento").value="";
+    document.getElementById("edadExacta").textContent="";
+    document.getElementById("edadExperimental").textContent="";
+    document.getElementById("empresa").value="";
+    document.getElementById("op1").checked=true;
+    document.getElementById("op2").checked=false;
+    document.getElementById("op3").checked=false;
 }
